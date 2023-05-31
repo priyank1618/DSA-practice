12b91ecae78aa71af26856f53efb1afe268d83b6
priyank <patelpriyank3112002@gmail.com>
2023-05-31 07:26:59 +0530

    ..

--- recursion/array/reverse.cpp
+++ recursion/array/reverse.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 
 
+void reverse(int arr[],int n)
+{
+   if(n==0) return ;
 
 
+   //tukde tukde ma array moklvo
+   reverse(arr+1,n-1);
 
+   //last tukda thi print karvo
+   cout << arr[0]<< " "; 
+}
 
 int main()
 {
-    
+   int arr[5]={1,2,3,4,5} ;
+   int n=5;
+
+   reverse(arr,n);
 }
